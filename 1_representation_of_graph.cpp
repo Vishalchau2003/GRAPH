c244fbdb92b7137dc293685d5dea47213ebaad91
Vishalchau2003 <cse23146@glbitm.ac.in>
2026-02-03 12:17:36 +0530

    Add files via upload

--- 1_representation_of_graph.cpp
+++ 1_representation_of_graph.cpp
@@ -18,22 +18,27 @@
 // }
 
 
+
 //by list
-#include<iostream>
-using namespace std;
 
-int main(){
-    int n,m;
-    cin>>n>>m;
-    //vector<vector<int>>adj(n+1);
-    //or
 
-    vector<int>adj[n+1];
-    for(int i=0;i<m;i++){
-       int u,v;
-       cin>>u>>v;
-       adj[u].push_back(v);
-       adj[v].push_back(u);
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     //vector<vector<int>>adj(n+1);
+//     //or
+
+//     vector<int>adj[n+1];
+//     for(int i=0;i<m;i++){
+//        int u,v;
+//        cin>>u>>v;
+//        adj[u].push_back(v);
+//        adj[v].push_back(u);
+//     }
+//     return 0;
+// }
+
+\
