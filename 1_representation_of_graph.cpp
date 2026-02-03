a64955eeaa29856ce62a8f42835a254a16eafb55
Vishalchau2003 <cse23146@glbitm.ac.in>
2026-02-03 12:00:59 +0530

    Add files via upload

---
+++ 1_representation_of_graph.cpp
@@ -0,0 +1,39 @@
+//by adjacency matrix
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     int adj[n+1][n+1];
+//     for(int i=0;i<m;i++){
+//        int u,v;
+//        cin>>u>>v;
+//        adj[u][v]=1;
+//        adj[v][u]=1;
+//     }
+//     return 0;
+// }
+
+
+//by list
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    //vector<vector<int>>adj(n+1);
+    //or
+
+    vector<int>adj[n+1];
+    for(int i=0;i<m;i++){
+       int u,v;
+       cin>>u>>v;
+       adj[u].push_back(v);
+       adj[v].push_back(u);
+    }
+    return 0;
+}
